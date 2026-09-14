bd2a1af001a561a9f0c7d92292be6561d1419af1
JADDEDA SRIKAR <srikaranitsa83@gmail.com>
2026-09-14 15:47:37 +0530

    Add files via upload

---
+++ Record_Tasks/arrays.js
@@ -0,0 +1,13 @@
+// JavaScript Program using Arrays
+
+// Create an array of 10 student marks
+let marks = [85, 72, 90, 65, 78, 95, 88, 60, 75, 92];
+
+// Display all marks
+console.log("Student Marks:");
+console.log(marks);
+
+// Display each mark one by one
+for (let i = 0; i < marks.length; i++) {
+    console.log("Student " + (i + 1) + ": " + marks[i]);
+}
---
+++ Record_Tasks/functions.js
@@ -0,0 +1,27 @@
+// JavaScript Program using Functions
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Function to subtract two numbers
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+// Function to multiply two numbers
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Function to divide two numbers
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+// Call the functions and display results
+console.log("Addition: " + addNumbers(20, 10));
+console.log("Subtraction: " + subtractNumbers(20, 10));
+console.log("Multiplication: " + multiplyNumbers(20, 10));
+console.log("Division: " + divideNumbers(20, 10));
