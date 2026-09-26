f9a92597ca714cd8276525ba6f2c66cbf62b2f27
JADDEDA SRIKAR <srikaranitsa83@gmail.com>
2026-09-26 16:14:43 +0530

    Add files via upload

---
+++ Observation_Tasks/Task6/app.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+// Display messages using the Chalk package
+console.log(chalk.green("Hello! Welcome to Node.js."));
+console.log(chalk.blue("This message is displayed using Chalk."));
+console.log(chalk.yellow("NPM successfully installed the external package."));
